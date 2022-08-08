524697a5ad5f8d75da45d1699c9243d5d0e8938a
dswwfg <dswwfg@naver.com>
2022-08-08 19:50:19 +0900

    Update 15.3sum.py
    - brute force 아닌 two pointer 이용한 방법
    - review 필요

--- 15.3sum.py
+++ 15.3sum.py
@@ -1,18 +1,50 @@
 
-# nums = [-1, 0, 1, 2, -1, 4]
+nums = [-1, 0, 1, 2, -1, -4]
 # nums = [0, 1, 1]
-nums = [0, 0, 0]
+# nums = [0, 0, 0]
 
 
+results = []
 nums.sort()
 
 print(nums)
 
 result = []
-for i in range(0, len(nums)-2):
-    for j in range(i+1, len(nums)-1):
-        for k in range(j+1, len(nums)):
-            if nums[i]+nums[j]+nums[k] == 0 and not [nums[i], nums[j], nums[k]] in result:
-                result.append([nums[i], nums[j], nums[k]])
 
-print('result: ', result)
+for i in range(0, len(nums)-2):
+
+
+    if i>0 and nums[i] == nums[i-1]:
+        continue
+    idx_l, idx_r = i+1, len(nums)-1
+
+    while idx_l < idx_r:
+        sum = nums[i] + nums[idx_l] + nums[idx_r]
+
+
+        if sum < 0:
+            idx_l += 1
+        elif sum > 0:
+            idx_r -= 1
+        else:
+            result.append([nums[i], nums[idx_l], nums[idx_r]])
+
+            while nums[idx_l] == nums[idx_l+1]:
+                idx_l +=1
+
+            while nums[idx_r] == nums[idx_r-1]:
+                idx_r -=1
+
+            idx_l += 1
+            idx_r -= 1
+
+print(result)
+
+
+
+
+
+
+
+
+

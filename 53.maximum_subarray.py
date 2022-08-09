a772f50a0889a1ecb5f4a0aecbfaeabb0ee79ca8
Gavin <dswwfg@naver.com>
2022-08-09 15:14:06 +0900

    re: 53.maximum_subarray.py
    Reference
    - https://sustainable-dev.tistory.com/23
    - https://www.youtube.com/watch?v=5WZl3MMT0Eg

--- 53.maximum_subarray.py
+++ 53.maximum_subarray.py
@@ -9,15 +9,56 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 '''
 from typing import List
 
-nums = [-2,1,-3,4,-1,2,1,-5,4]
+temp = [-2, 1, -3, 4]
+
+nums = [-2, -3, 4, -1, -2, 1, 5, -3]
 
 
-def maxSubArray(nums: List[int]) -> int:
-    for i in range(1, len(nums)):
-        if nums[i - 1] > 0:
-            nums[i] += nums[i - 1]
-    return max(nums)
+# 1. bruteforce
+# def maxSubArray(nums: List[int]) -> int:
+#     # 모든 subarray 구하기
+#     maximum = []
+#
+#     for i in range(len(nums)):
+#         comb = []
+#         for j in range(i+1, len(nums) + 1):
+#             comb.append(nums[i:j])
+#         max_local = max([sum(x) for x in comb])
+#
+#         maximum.append(max_local)
+#     return max(maximum)
+#
+# print(maxSubArray(nums))
 
-print(maxSubArray(nums))
+# 2. kadene's algorithm
+nums = [-3, 2, 5, -1]
 
-# Kadane Algorithm
+local_max = nums[0]
+global_max = nums[0]
+
+for i in range(1, len(nums)):
+    local_max = max(nums[i], local_max+nums[i])
+    print('local_max: ', local_max)
+
+    if local_max >= global_max:
+        global_max = local_max
+
+print('global_max: ', global_max)
+
+
+
+
+# max_sub = -10000 - 1
+# max_ending = 0
+#
+# for i in range(0, len(a)):
+#     max_ending = max_ending + a[i]
+#     if max_sub < max_ending:
+#         max_sub = max_ending
+#
+#     if max_ending < 0:
+#         max_ending = 0
+#
+# print(max_sub)
+
+

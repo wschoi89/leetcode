6c683eeff8417b7d8cc8dc5ad28917ab11d83f57
Gavin <dswwfg@naver.com>
2022-08-09 16:44:45 +0900

    Finish: 88.merge_sorted_array.py

--- 88.merge_sorted_array.py
+++ 88.merge_sorted_array.py
@@ -24,25 +24,38 @@ nums2 = [2, 5, 6], n = 3
 Output: [1, 2, 2, 3, 5, 6]
 '''
 
-def merge(nums1, m, nums2, n):
-    while m > 0 and n > 0:
-        if nums1[m-1] <= nums2[n-1]:
-            nums1[m+n-1] = nums2[n-1]
-            n -=1
-        else:
-            nums1[m+n-1] = nums1[m-1]
-            m -=1
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
 
-    if n>0:
-        nums1[:n] = nums2[:n]
+# for i in range(m+n-1, -1, -1):
+#
+#     if m>=1 and n>=1:
+#         if nums1[m-1] >= nums2[n-1]:
+#             nums1[i] = nums1[m-1]
+#             m -=1
+#         else:
+#             nums1[i] = nums2[n-1]
+#             n -=1
+#
+#     if m>=1:
+#        pass
+#
+#     elif n>=1:
+#         nums1[i] = nums2[n-1]
+#         n -=1
 
-nums1 = [0]
-m = 0
-nums2 = [1]
-n = 1
+while m>0 and n>0:
+    if nums2[n-1] >= nums1[m-1]:
+        nums1[m+n-1] = nums2[n-1]
+        n -=1
+    else:
+        nums1[m+n-1] = nums1[m-1]
+        m -=1
 
-merge(nums1, m, nums2, n)
-print(nums1)
+nums1[:n] = nums2[:n]
+
 
 
 

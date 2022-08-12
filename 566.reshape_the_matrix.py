e89abb12b9a555bae5cb48954561bccda267f82e
dswwfg <dswwfg@naver.com>
2022-08-12 12:35:09 +0900

    Finish: 566.reshape_the_matrix.py

--- 566.reshape_the_matrix.py
+++ 566.reshape_the_matrix.py
@@ -3,13 +3,16 @@ unpack_before = [[1, 2, 3], [4, 5, 6]]
 def reshape_matrix(mat, r, c):
     m, n = len(mat), len(mat[0])
 
-    if m * n != r * c:
+    if m*n != r*c:
         return mat
 
-    ans = [[0] * c for _ in range(r)]
-    print('ans: ', ans)
-    for i in range(m * n):
-        ans[i // c][i % c] = mat[i // n][i % n]
+    reshaped = [[0]*c for _ in range(r)]
+    print(reshaped)
 
-    return ans
+    for i in range(m*n):
+        reshaped[i//c][i%c] = mat[i//n][i%n]
+    print(reshaped)
+
+
+reshape_matrix(unpack_before, 3, 2)
 

9c8433b684e518000f5576f73fbbdee702828327
dswwfg <dswwfg@snu.ac.kr>
2022-08-17 16:57:38 +0900

    re: 36.valid_sudoku.py
    - 풀었지만 row를 제외하고 column, 3x3 box 체크하는부분 오래걸림

--- 36.valid_sudoku.py
+++ 36.valid_sudoku.py
@@ -1,47 +1,76 @@
-board = \
-[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# board = \
+# [["5","3",".",".","7",".",".",".","."],
+#  ["6",".",".","1","9","5",".",".","."],
+#  [".","9","8",".",".",".",".","6","."],
+#  ["8",".",".",".","6",".",".",".","3"],
+#  ["4",".",".","8",".","3",".",".","1"],
+#  ["7",".",".",".","2",".",".",".","6"],
+#  [".","6",".",".",".",".","2","8","."],
+#  [".",".",".","4","1","9",".",".","5"],
+#  [".",".",".",".","8",".",".","7","9"]]
 
+# board = \
+#     [[".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."],
+#      [".",".",".",".",".",".",".",".","."]]
+
+
+board = \
+[[".",".",".",".",".",".","5",".","."],
+ [".",".",".",".",".",".",".",".","."],
+ [".",".",".",".",".",".",".",".","."],
+ ["9","3",".",".","2",".","4",".","."],
+ [".",".","7",".",".",".","3",".","."],
+ [".",".",".",".",".",".",".",".","."],
+ [".",".",".","3","4",".",".",".","."],
+ [".",".",".",".",".","3",".",".","."],
+ [".",".",".",".",".","5","2",".","."]]
+from collections import Counter
+
+# print(board)
 # dict 로 진행
 def isValidSudoku(board) -> bool:
-    n_rows = 9
-    n_cols = 9
+    for row in board:
+        counter = Counter(row)
+        # print(counter)
 
-    # row 검증
-    for r in range(n_rows):
-        dict_ = dict()
-        for c in range(n_cols):
-            if board[r][c].isnumeric():
-                dict_[board[r][c]] = dict_.get(board[r][c], 0) + 1
+        most_common = counter.most_common(2)
+        print(len(most_common))
 
-                if dict_[board[r][c]] == 2:
-                    return False
+        if len(most_common) == 1 and most_common[0][0] == '.':
+            continue
+        if most_common[1][1] != 1:
+            return False
 
-        # 9칸 검증
-        if r % 3 == 2:
-            for iter in range(3):
-                dict_9 = dict()
+        # rule2 (column check)
 
-                for sub_r in range(r-2, r+1):
-                    for sub_c in range(3*iter, 3*iter+3):
-                        if board[sub_r][sub_c].isnumeric():
-                            dict_9[board[sub_r][sub_c]] = dict_9.get(board[sub_r][sub_c], 0) + 1
+    columns = [[row[x] for row in board] for x in range(9)]
 
-                            if dict_9[board[sub_r][sub_c]] == 2:
-                                return False
+    for column in columns:
+        counter = Counter(column)
+        most_common = counter.most_common(2)
+        if len(most_common) == 1 and most_common[0][0] == '.':
+            continue
+        if most_common[1][1] != 1:
+            return False
 
-    # column 검증
-    for c in range(n_cols):
-        dict_ = dict()
-        for r in range(n_rows):
-
-            if board[r][c].isnumeric():
-                dict_[board[r][c]] = dict_.get(board[r][c], 0) + 1
-
-                if dict_[board[r][c]] == 2:
-                    return False
+    for c in range(0, 9, 3):
+        for r in range(0, 9, 3):
+            box = [board[i][j] for i in range(0 + c, 3 + c) for j in range(0 + r, 3 + r)]
+            counter = Counter(box)
+            most_common = counter.most_common(2)
+            if len(most_common) == 1 and most_common[0][0] == '.':
+                continue
+            if most_common[1][1] != 1:
+                return False
 
     return True
-
 
 
 print(isValidSudoku(board))

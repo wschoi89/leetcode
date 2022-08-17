efeab503aef9e6e2dec5fc55014e8fc1f7dee179
dswwfg <dswwfg@snu.ac.kr>
2022-08-17 15:30:29 +0900

    Finish: 118.Pascal's_triangle.py

--- 118.Pascal's_triangle.py
+++ 118.Pascal's_triangle.py
@@ -1,10 +1,17 @@
-def generate(numRows: int):
-    result = [[1] * i for i in range(1, numRows+1)]
+from typing import List
 
-    for i in range(2, numRows):
+
+def generate(num_rows: int)->List[List[int]]:
+
+    result = [[1]*i for i in range(1, num_rows+1)]
+    print(result)
+
+    if num_rows <=2:
+        return result
+
+    for i in range(2, num_rows):
         for j in range(1, i):
-            result[i][j] = result[i-1][j] + result[i-1][j-1]
+            result[i][j] = result[i-1][j-1] + result[i-1][j]
+    print(result)
 
-    return result
-
-print(generate(4))
+generate(5)

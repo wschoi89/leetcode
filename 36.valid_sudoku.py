4fdc8e919316e684cdf79304aac25ee7be767c89
dswwfg <dswwfg@naver.com>
2022-08-19 01:21:02 +0900

    re: valid_sudoku
    - tuple이 dictionary의 key가 될 수도 있음

--- 36.valid_sudoku.py
+++ 36.valid_sudoku.py
@@ -1,79 +1,38 @@
-# board = \
-# [["5","3",".",".","7",".",".",".","."],
-#  ["6",".",".","1","9","5",".",".","."],
-#  [".","9","8",".",".",".",".","6","."],
-#  ["8",".",".",".","6",".",".",".","3"],
-#  ["4",".",".","8",".","3",".",".","1"],
-#  ["7",".",".",".","2",".",".",".","6"],
-#  [".","6",".",".",".",".","2","8","."],
-#  [".",".",".","4","1","9",".",".","5"],
-#  [".",".",".",".","8",".",".","7","9"]]
-
-# board = \
-#     [[".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."],
-#      [".",".",".",".",".",".",".",".","."]]
-
-
+# 3rules
+from typing import List
+from collections import defaultdict
 board = \
-[[".",".",".",".",".",".","5",".","."],
- [".",".",".",".",".",".",".",".","."],
- [".",".",".",".",".",".",".",".","."],
- ["9","3",".",".","2",".","4",".","."],
- [".",".","7",".",".",".","3",".","."],
- [".",".",".",".",".",".",".",".","."],
- [".",".",".","3","4",".",".",".","."],
- [".",".",".",".",".","3",".",".","."],
- [".",".",".",".",".","5","2",".","."]]
-from collections import Counter
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
 
-# print(board)
-# dict 로 진행
-def isValidSudoku(board) -> bool:
-    for row in board:
-        counter = Counter(row)
-        # print(counter)
+def isValidSudoku(board: List[List[str]])->bool:
+    dict_rows = defaultdict(set)
+    dict_cols = defaultdict(set)
+    dict_box = defaultdict(set)
 
-        most_common = counter.most_common(2)
-        print(len(most_common))
+    rows, cols = len(board), len(board[0])
 
-        if len(most_common) == 1 and most_common[0][0] == '.':
-            continue
-        if most_common[1][1] != 1:
-            return False
-
-        # rule2 (column check)
-
-    columns = [[row[x] for row in board] for x in range(9)]
-
-    for column in columns:
-        counter = Counter(column)
-        most_common = counter.most_common(2)
-        if len(most_common) == 1 and most_common[0][0] == '.':
-            continue
-        if most_common[1][1] != 1:
-            return False
-
-    for c in range(0, 9, 3):
-        for r in range(0, 9, 3):
-            box = [board[i][j] for i in range(0 + c, 3 + c) for j in range(0 + r, 3 + r)]
-            counter = Counter(box)
-            most_common = counter.most_common(2)
-            if len(most_common) == 1 and most_common[0][0] == '.':
+    for r in range(rows):
+        for c in range(cols):
+            if board[r][c] == '.':
                 continue
-            if most_common[1][1] != 1:
-                return False
 
+            if (board[r][c] in dict_rows[r]) or (board[r][c] in dict_cols[c]) or (board[r][c] in dict_box[(r // 3,c // 3)]):
+                return False
+            else:
+                dict_rows[r].add(board[r][c])
+                dict_cols[c].add(board[r][c])
+                dict_box[(r//3,c//3)].add(board[r][c])
+
+    print(dict_box)
     return True
 
 
 print(isValidSudoku(board))
-
-
-

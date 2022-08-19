514419b5ae29a841569b1d15d61cef72a581f9f9
dswwfg <dswwfg@naver.com>
2022-08-19 16:27:58 +0900

    re: 238.product_of_array_except_self.py
    - 어렵지만 반복적으로 시도필요

--- 238.product_of_array_except_self.py
+++ 238.product_of_array_except_self.py
@@ -5,19 +5,17 @@ Output: [24,12,8,6]
 
 nums = [1, 2, 3, 4]
 
-result = [0 for _ in range(len(nums))]
+tmp = 1
+result = []
+for num in nums:
+    result.append(tmp)
+    tmp *= num
+
+tmp = 1
+for idx in range(len(nums) - 1, -1, -1):
+    result[idx] = result[idx] * tmp
+    tmp *= nums[idx]
+
 print(result)
-
-for i in range(len(result)):
-    temp = 1
-    for j in range(len(result)):
-        if i == j:
-            continue
-
-        temp *= nums[j]
-    result[i] = temp
-
-print(result)
-
 
 

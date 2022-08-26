0226c3097f9d177158149623916fee3aa96a0b7a
dswwfg <dswwfg@snu.ac.kr>
2022-08-26 16:33:17 +0900

    feat: Linked List 클래스 추가

--- coding_interview/utils.py
+++ coding_interview/utils.py
@@ -1,5 +1,33 @@
 import time
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, value):
+        node = ListNode(value)
+
+        if self.head is None:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+    def print(self):
+        cur = self.head
+
+        while cur:
+            print(cur.val, end=" ")
+            cur = cur.next
+        print()
+
 
 def my_timer(func):
     def wrapper(s):
@@ -7,3 +35,14 @@ def my_timer(func):
         print(func(s))
         print(f" {func.__name__} 실행 시간: {time.perf_counter() - t_start}")
     return wrapper
+
+
+
+def print_linked_list(head_node: ListNode):
+    cur = head_node
+    while cur:
+        print(cur.val, end=" ")
+        cur = cur.next
+
+
+

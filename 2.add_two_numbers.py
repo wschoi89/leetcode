7e6a82f1e0c83dece589b7123653d6aa8a46a7b3
Gavin <dswwfg@naver.com>
2022-09-22 13:43:17 +0900

    re: 2.add_two_numbers.py
    - youtube 설명을 보니 직관적
    - 개별 test case를 나눠서 생각해보면서 code 개선

--- 2.add_two_numbers.py
+++ 2.add_two_numbers.py
@@ -19,56 +19,27 @@ l2 = LinkedList()
 l2.inserts([5, 6, 4])
 l2.print()
 
-l1_list = []
-l2_list = []
 
-cur = l1.head
-while cur:
-    l1_list.append(cur.val)
-    print(l1_list)
-    cur = cur.next
+def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
+    dummy = ListNode()
+    cur = dummy
+    carry = 0
+
+    while l1 or l2 or carry:
+        val_1 = l1.val if l1 else 0
+        val_2 = l2.val if l2 else 0
+
+        val = val_1 + val_2 + carry
+        carry = val // 10
+        val = val % 10
+        cur.next = ListNode(val)
+        cur = cur.next
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+
+    return dummy.next
 
 
-cur = l2.head
-while cur:
-    l2_list.append(cur.val)
-    # print(l2_list)
-    cur = cur.next
-
-print(pow(10, 0))
-print(pow(10, 1))
-print(pow(10, 2))
-l1_value = 0
-l2_value = 0
-
-for i, v in enumerate(l1_list):
-    print(i, v)
-    print(pow(10, i) * v)
-    l1_value += pow(10, i) * v
-
-for i, v in enumerate(l2_list):
-    print(i, v)
-    print(pow(10, i) * v)
-    l2_value += pow(10, i) * v
-
-print(l1_value)
-print(l2_value)
-
-# print('result: ', l1_value + l2_value)
-
-result = l1_value + l2_value
-print('result:', result)
-
-print('hello', [i for i in str(result)])
-
-list_ = [i for i in str(result)]
-
-node = ListNode(int(list_[-1]))
-cur = node
-for i in range(0, len(list_)-1):
-    new_node = ListNode(list_[-1-1-i])
-
-    cur.next = new_node
-    cur = new_node
-
-node.print_all()
+result = add_two_numbers(l1.head, l2.head)
+result.print_all()

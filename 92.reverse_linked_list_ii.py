deb75ca00fb6ce629cf8d50eadb73173f2ea33ce
dswwfg <dswwfg@snu.ac.kr>
2022-10-03 23:57:22 +0900

    re: 92.reverse_linked_list_ii.py
    - 코드의 통일성 및 edge case를 처리하기 위해 dummy 노드 사용

---
+++ 92.reverse_linked_list_ii.py
@@ -0,0 +1,33 @@
+from coding_interview.utils import LinkedList, ListNode
+from typing import Optional
+
+l1 = LinkedList()
+l1.inserts([1,2,3,4,5])
+
+left, right = 2, 4
+
+def reverseBetween(self, head: Optional[ListNode], left: int, right: int):
+    # phase 1
+    dummy = ListNode(0, head)
+
+    left_prev, cur = dummy, head
+    for _ in range(left - 1):
+        left_prev = cur
+        cur = cur.next
+    
+    # phase 2
+    # now cur="left", leftPrev="node before left"
+    # reverse from left to right
+    prev = None
+    for i in range(right - left + 1):
+        tmpNext = cur.next
+        cur.next = prev
+        prev = cur
+        cur = tmpNext
+
+    # phase 3
+    # update pointers
+    left_prev.next.next = cur
+    left_prev.next = prev
+
+    return dummy.next

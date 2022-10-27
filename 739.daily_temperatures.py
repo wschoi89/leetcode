2f633778f926fe27692042b416a0adacaad77e21
Gavin <dswwfg@naver.com>
2022-10-27 12:10:38 +0900

    re: 739.daily_temperatures.py
    - stack을 이용해 스스로 풀어봄
    - 어떤 문제에 stack을 사용해야하는지 판단 근거 필요

--- 739.daily_temperatures.py
+++ 739.daily_temperatures.py
@@ -1,19 +1,31 @@
+from typing import List, Optional
+
 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
 # output = [1, 1, 1, 0]
 
 
-def dailyTemperatures(temperatures):
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
 
     result = [0] * len(temperatures)
+
+    stack = []
+
     for i, t in enumerate(temperatures):
-        for j in range(i+1, len(temperatures)):
-            if temperatures[j] > t:
-                result[i] = j - i
-                break
 
-    print(result)
+        while stack and stack[-1][0] < t:
+            result[stack[-1][1]] = i - stack[-1][1]
+            stack.pop()
 
-dailyTemperatures(temperatures)
+        else:
+            stack.append([t, i])
+
+    return result
+
+
+
+
+result = dailyTemperatures(temperatures)
+print('result: ', result)
 
 
 

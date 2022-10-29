30cdc14243d89c7a971c466e5244df6c752844e8
dswwfg <dswwfg@naver.com>
2022-10-30 02:23:05 +0900

    finish : 316.remove_duplicate_letters.py

--- 316.remove_duplicate_letters.py
+++ 316.remove_duplicate_letters.py
@@ -1,27 +1,30 @@
-s = "abacb"
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
 
-from collections import Counter
+        from collections import Counter
 
-# pairs = {"a":1, "b":2, "c":2}
-pairs = Counter(s)
-print('pairs: ', pairs)
+        # pairs = {"a":1, "b":2, "c":2}
+        pairs = Counter(s)
+        print('pairs: ', pairs)
 
-# print([k>'b' for k, v in pairs.items() if pairs[k]==1])
-# print(all([k>'b' for k, v in pairs.items() if pairs[k]==1]))
+        stack = []
 
-stack = []
+        for ch in s:
 
-for ch in s:
+            pairs[ch] -= 1
 
-    pairs[ch] -= 1
+            if not ch in stack:
 
-    if not ch in stack:
+                while stack and stack[-1] > ch and pairs[stack[-1]] != 0:
+                    stack.pop()
 
-        while stack and stack[-1] > ch and pairs[stack[-1]] != 0:
-            stack.pop()
+            if ch in stack:
+                continue
+            stack.append(ch)
 
-    if ch in stack:
-        continue
-    stack.append(ch)
+        return ''.join(stack)
 
-print(stack)
+
+s = "cbacdcbc"
+result = Solution().removeDuplicateLetters(s)
+print(result)  #
